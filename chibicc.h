25094ffc5ad97a45a56b9b7dbdb7a1d96bbdacd1
nikekukuri <nikekukuri0327@gmail.com>
2024-02-15 23:03:19 +0900

    Support zero-arity function difinition

--- chibicc.h
+++ chibicc.h
@@ -56,6 +56,8 @@ struct Obj {
 // Function
 typedef struct Function Function;
 struct Function {
+	Function *next;
+	char *name;
 	Node *body;
 	Obj *locals;
 	int stack_size;
@@ -127,6 +129,7 @@ void codegen(Function *node);
 typedef enum {
 	TY_INT,
 	TY_PTR,
+	TY_FUNC,
 } TypeKind;
 
 struct Type {
@@ -137,10 +140,14 @@ struct Type {
 
 	// Declaration
 	Token *name;
+
+	// Function type
+	Type *return_ty;
 };
 
 extern Type *ty_int;
 
 bool is_integer(Type *ty);
 Type *pointer_to(Type *base);
+Type *func_type(Type *return_ty);
 void add_type(Node *node);
